4fb8bd5d203ecb054cb35842ff11963a79deac4e
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-30 15:44:10 +0530

    added course model

---
+++ src/models/course.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+const courseSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    code:{
+        type:String,
+        required:true
+    },
+    teacher:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users"
+    },
+    students:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"users"
+    }]
+},{timestamps:true});
+
+export const courseModel = mongoose.model("course",courseSchema);
