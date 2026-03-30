fb3c3136728e0c8cf2a84bb68276d92d42f0b18f
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-30 16:10:22 +0530

    added class model

---
+++ src/models/class.mode.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const classSchema = new mongoose.Schema({
+  name:{
+    type:String,
+    unique:true
+  },
+
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  students: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  }],
+
+  // 🔥 GPS
+  location: {
+    latitude: Number,
+    longitude: Number,
+    radius: {
+      type: Number,
+      default: 30,
+    }
+  }
+
+}, { timestamps: true });
+
+export const classModel = mongoose.model("class",classSchema);
