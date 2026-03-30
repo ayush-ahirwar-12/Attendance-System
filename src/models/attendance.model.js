81741ac0e42621266329c39651d5b1c71d39eb2c
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-30 15:47:53 +0530

    added attendace model

---
+++ src/models/attendance.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const attendanceSchema = new mongoose.Schema({
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "course",
+  },
+
+  qrSession: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "qr",
+  },
+
+  // 🔥 Face verification result
+  faceMatched: {
+    type: Boolean,
+    default: false,
+  },
+
+  faceDistance: {
+    type: Number, // store distance for debugging
+  },
+
+  // 🔥 GPS verification
+  location: {
+    latitude: Number,
+    longitude: Number,
+  },
+
+  isWithinRadius: {
+    type: Boolean,
+    default: false,
+  },
+
+  status: {
+    type: String,
+    enum: ["present", "absent"],
+    default: "absent",
+  }
+
+}, { timestamps: true });
+
+export const attendanceModel = mongoose.model("attendance",attendanceSchema);
