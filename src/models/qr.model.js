ca91f3d4c15ad30d44cf12e3c8fb0aad43387a54
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-30 15:45:31 +0530

    added qrsession model

---
+++ src/models/qr.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const qrSessionSchema = new mongoose.Schema({
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "course",
+  },
+
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+
+  qrToken: {
+    type: String,
+    required: true,
+  },
+
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+
+  isActive: {
+    type: Boolean,
+    default: true,
+  }
+
+}, { timestamps: true });
+
+export const qrSessionModel = mongoose.model("qr",qrSessionSchema);
+
