15118a701b3cb9689bab596a394ae9e42007c62e
ayush ahirwar <ayushahirwar04@gmail.com>
2026-05-09 16:08:21 +0530

    added semester contract

---
+++ src/repositories/contracts/ISemesterRepository.js
@@ -0,0 +1,20 @@
+class iSemesterRepository {
+
+  async createSemester(semesterData) {
+    throw new Error("Method not implemented.");
+  }
+
+  async findById(semesterId) {
+    throw new Error("Method not implemented.");
+  }
+
+  async findAll() {
+    throw new Error("Method not implemented.");
+  }
+
+  async updateStatus(semesterId, status) {
+    throw new Error("Method not implemented.");
+  }
+
+
+}
